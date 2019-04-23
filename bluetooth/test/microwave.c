9b6d28ab1d77efb0d60f651eb6aea8b43231cd51
gyelokhan <gyelok.han@gmail.com>
2019-04-23 14:55:23 +0900

    first commit

---
+++ bluetooth/test/microwave.c
@@ -0,0 +1,40 @@
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+int main (int argc, char *argv[])
+{
+	int trig = 2 ;
+	int echo = 8 ;
+	float start_time, end_time ;
+	float distance ;
+
+	if (wiringPiSetup() == -1)
+	{
+		printf ("[%s:%d] fail to call wiringPiSetup function: %s \n", __func__, __LINE__, strerror(errno));
+		exit(1) ;
+	}
+
+	pinMode(trig, OUTPUT);
+	pinMode(echo , INPUT);
+
+	while (1)
+	{
+		digitalWrite (trig, LOW);  // Init Trigger
+		delay (1000);	// 1 sec
+		digitalWrite(trig, HIGH) ;	// send signal
+		delayMicroseconds(10) ;		// 10 * 0.000001 sec
+		digitalWrite(trig, LOW) ;	// receive signal
+
+		while (digitalRead(echo) == 0) ;	
+		start_time = micros() ;	// get microseconds after program runs
+		
+		while (digitalRead(echo) == 1) ;
+		end_time = micros() ;
+
+		distance = (end_time - start_time) / 29. / 2. ;	// Ultrasound is 1cm per 29.1 microsecond, 2 is roundtrip.
+		printf("distance %.2f cm\n", distance) ;
+	}
+}
