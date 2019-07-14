7ed010c952e427313d45eb61e4df0ffc9fcf023d
gyelokhan <gyelok.han@gmail.com>
2019-07-14 22:02:41 +0900

    just modify compile error

--- ipcamera/app/views.py
+++ ipcamera/app/views.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from pivideostream import PiVideoStream
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
